269e15adeac8b5214c8db8d863373b3f71c95de8
ahuang100 <andyhuang715@gmail.com>
2024-04-24 18:13:16 -0700

    first block manually added, reward mined success

--- blockchain.py
+++ blockchain.py
@@ -25,7 +25,13 @@ class Blockchain:
         self.mining_reward = mining_reward
         self.chain = []
         self.current_transactions = []
-
+    
+        # manually added first block to the chain so adding another block is possible since it requires hash of prev block
+        first_block = self.create_block(1, [], 0, "0")
+        while not self.check_proof(first_block):
+            first_block.proof += 1
+        self.chain.append(first_block)
+    
     def create_block(self, index, transactions, proof, previous_hash):
         return Block(index, copy.copy(transactions), proof, previous_hash)
 
@@ -56,11 +62,35 @@ class Blockchain:
 
     def check_proof(self, block):
         # Check that the hash of the block ends in difficulty_number many zeros
-        return False
+        hashed = self.hash_block(block)
+        checkzeros = hashed[-self.difficulty_number:]
+        for i in checkzeros:
+            if i != '0':
+                return False
+        return True
 
     def mine(self):
         # Give yourself a reward at the beginning of the transactions
+        reward = self.create_transaction("Block reward", self.address, self.mining_reward)
+        self.current_transactions.insert(0, reward)
+
         # Find the right value for proof
+        prevblock = self.current_block()
+        prevhashvalue = self.hash_block(prevblock)
+
+        powguess = 0 
+        while(True):
+            testblock = self.create_block(self.next_index(), self.current_transactions, powguess, prevhashvalue)
+            if(self.check_proof(testblock)):
+                break
+            powguess += 1
+        
         # Add the block to the chain
+        self.add_block(testblock)
+
         # Clear your current transactions
-        pass
+        self.current_transactions = []
+
+
+
+# (_)
